d1242d9b7c6b3295b03d78ae871485ddc4faa3bf
abhi <abhiramk@stud.ntnu.no>
2024-04-14 16:23:00 +0200

    implemented constuctor, get-methods, add-method and subtract-method in Vector2D

--- src/main/java/no/ntnu/idatt2003/chaosgame/Vector2D.java
+++ src/main/java/no/ntnu/idatt2003/chaosgame/Vector2D.java
@@ -1,4 +1,32 @@
 package no.ntnu.idatt2003.chaosgame;
 
 public class Vector2D {
+    private double x0;
+    private double x1;
+
+    public Vector2D(double x0, double x1) {
+        this.x0 = x0;
+        this.x1 = x1;
+    }
+
+    public double getX0() {
+        return x0;
+    }
+
+    public double getX1() {
+        return x1;
+    }
+
+    public Vector2D add(Vector2D other){
+        double newX0 = this.x0 + other.getX0();
+        double newX1 = this.x1 + other.getX1();
+        return new Vector2D(newX0, newX1);
+    }
+    public Vector2D subtract(Vector2D other){
+        double newX0 = this.x0 - other.getX0();
+        double newX1 = this.x1 - other.getX1();
+        return new Vector2D(newX0, newX1);
+    }
 }
+
+

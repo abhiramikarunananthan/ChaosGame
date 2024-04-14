b88ba45d0cc89cfd149fe2375e5e035f95ad5327
abhi <abhiramk@stud.ntnu.no>
2024-04-14 16:52:24 +0200

    implemented constructor and sqrt-method in Complex class

--- src/main/java/no/ntnu/idatt2003/chaosgame/Complex.java
+++ src/main/java/no/ntnu/idatt2003/chaosgame/Complex.java
@@ -1,4 +1,15 @@
 package no.ntnu.idatt2003.chaosgame;
+public class Complex extends Vector2D {
 
-public class Complex {
+    public Complex(double realPart, double imaginaryPart) {
+        super(realPart, imaginaryPart);
+    }
+
+
+    public Complex sqrt(){
+       double newRealPart  = Math.sqrt(0.5*(Math.sqrt(Math.pow(this.getX0(), 2) +Math.pow(this.getX1(), 2) ) + this.getX0()));
+       double newImaginaryPart = Math.sqrt(0.5*(Math.sqrt(Math.pow(this.getX0(), 2) +Math.pow(this.getX1(), 2) ) - this.getX0()));
+        return new Complex(newRealPart, newImaginaryPart);
+    }
+
 }

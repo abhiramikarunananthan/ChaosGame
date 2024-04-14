78bcc25158347225646752f1b163dd5848669ec9
abhi <abhiramk@stud.ntnu.no>
2024-04-14 18:38:38 +0200

    implemented transform method in AffineTransform2D

--- src/main/java/no/ntnu/idatt2003/chaosgame/AffineTransform2D.java
+++ src/main/java/no/ntnu/idatt2003/chaosgame/AffineTransform2D.java
@@ -1,8 +1,20 @@
 package no.ntnu.idatt2003.chaosgame;
 
 public class AffineTransform2D implements Transform2D {
+
+    private Matrix2x2 matrix;
+    private Vector2D vector;
+
+
+    public AffineTransform2D(Matrix2x2 matrix, Vector2D vector) {
+        this.matrix = matrix;
+        this.vector = vector;
+    }
+
     @Override
     public Vector2D transform(Vector2D point) {
-        return null;
+        Vector2D aX = this.matrix.multiply(point);
+        return aX.add(this.vector);
     }
+
 }

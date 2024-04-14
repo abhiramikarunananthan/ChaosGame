eaee75d93978619734cb7449f1039f233b7c24e0
abhi <abhiramk@stud.ntnu.no>
2024-04-14 17:01:05 +0200

    added JuliaTransform and implemented transform2D interface

---
+++ src/main/java/no/ntnu/idatt2003/chaosgame/JuliaTransform.java
@@ -0,0 +1,8 @@
+package no.ntnu.idatt2003.chaosgame;
+
+public class JuliaTransform implements Transform2D{
+    @Override
+    public Vector2D transform(Vector2D point) {
+        return null;
+    }
+}

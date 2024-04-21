5bc38173631d2e3002a2724da2c588a3e6dfd50a
abhi <abhiramk@stud.ntnu.no>
2024-04-21 15:25:15 +0200

    added ChaosGameDescription class with constructor and get-methods

---
+++ src/main/java/no/ntnu/idatt2003/chaosgame/ChaosGameDescription.java
@@ -0,0 +1,27 @@
+package no.ntnu.idatt2003.chaosgame;
+
+import java.util.List;
+
+public class ChaosGameDescription {
+    private List<Transform2D> transforms;
+    private Vector2D minCoords;
+    private Vector2D maxCoords;
+
+    public ChaosGameDescription(List<Transform2D> transforms, Vector2D minCoords, Vector2D maxCoords){
+        this.transforms = transforms;
+        this.minCoords = minCoords;
+        this.maxCoords = maxCoords;
+    }
+
+    public List<Transform2D> getTransforms() {
+        return transforms;
+    }
+
+    public Vector2D getMinCoords() {
+        return minCoords;
+    }
+
+    public Vector2D getMaxCoords() {
+        return maxCoords;
+    }
+}

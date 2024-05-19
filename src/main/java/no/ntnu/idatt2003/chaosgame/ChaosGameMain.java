33954d5d7f5ee20b6e4bfdd1f0d194d8d66b5143
abhi <abhiramk@stud.ntnu.no>
2024-05-19 22:33:17 +0200

    cleaned up and removed unnecessary code from ChaosGameMain class

--- src/main/java/no/ntnu/idatt2003/chaosgame/ChaosGameMain.java
+++ src/main/java/no/ntnu/idatt2003/chaosgame/ChaosGameMain.java
@@ -104,50 +104,6 @@ public class ChaosGameMain extends Application {
 
 
 
-/*
-        int width = 360;
-        int height = 120;
-
-        AffineTransform2D affineTransform2D1 = new AffineTransform2D(new Matrix2x2(0.5,0,0,0.5), new Vector2D(0, 0));
-        AffineTransform2D affineTransform2D2 = new AffineTransform2D(new Matrix2x2(0.5,0,0,0.5), new Vector2D(0.5, 0));
-        AffineTransform2D affineTransform2D3 = new AffineTransform2D(new Matrix2x2(0.5,0,0,0.5), new Vector2D(0.25, 0.5));
-
-        AffineTransform2D barnsley1 = new AffineTransform2D(new Matrix2x2(0,0,0,0.16), new Vector2D(0, 0));
-        AffineTransform2D barnsley2 = new AffineTransform2D(new Matrix2x2(0.85,0.04,-0.04,0.85), new Vector2D(0, 1.6));
-        AffineTransform2D barnsley3 = new AffineTransform2D(new Matrix2x2(0.2,-0.26,0.23,0.22), new Vector2D(0, 1.6));
-        AffineTransform2D barnsley4 = new AffineTransform2D(new Matrix2x2(-0.15,0.28,0.26,0.24), new Vector2D(0, 0.44));
-
-
-        List<Transform2D> listOfTranformation = new ArrayList<>();
-        listOfTranformation.add(affineTransform2D1);
-        listOfTranformation.add(affineTransform2D2);
-        listOfTranformation.add(affineTransform2D3);
-
-        List<Transform2D> listOfBarnsleyTransformations = new ArrayList<>();
-        listOfBarnsleyTransformations.add(barnsley1);
-        listOfBarnsleyTransformations.add(barnsley2);
-        listOfBarnsleyTransformations.add(barnsley3);
-        listOfBarnsleyTransformations.add(barnsley4);
-
-        ChaosGameDescription chaosGameDescription = new ChaosGameDescription(listOfTranformation, new Vector2D(0, 0), new Vector2D(width , height));
-        ChaosGame chaosGame = new ChaosGame(chaosGameDescription, height, width);
-        chaosGame.runSteps(50000);
-
-        for (int i = height; i >= 0; i--) {
-            for (int j = 0; j < width; j++) {
-                Vector2D vector = new Vector2D(j,i);
-                int point = chaosGame.getCanvas().getPixel(vector);
-
-                if(point == 0){
-                    System.out.print(" ");
-                } else {
-                    System.out.print("X");
-                }
-
-            }
-            System.out.println();
-        }
-*/
     }
 
 

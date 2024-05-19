87a3b64568c7121ee4f2bbc7bc7bb7c6fc70042c
abhi <abhiramk@stud.ntnu.no>
2024-05-19 22:34:15 +0200

    added code do add own fractal on MenuScene class

--- src/main/java/no/ntnu/idatt2003/chaosgame/scenes/MenuScene.java
+++ src/main/java/no/ntnu/idatt2003/chaosgame/scenes/MenuScene.java
@@ -1,12 +1,19 @@
 package no.ntnu.idatt2003.chaosgame.scenes;
 
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import no.ntnu.idatt2003.chaosgame.SceneController;
+import no.ntnu.idatt2003.chaosgame.components.ChaosGameDescription;
+import no.ntnu.idatt2003.chaosgame.data.ChaosGameFileHandler;
+import no.ntnu.idatt2003.chaosgame.transforms.Transformations;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class MenuScene {
@@ -20,14 +27,39 @@ public class MenuScene {
         Button startButton = new Button("START");
         Button importButton = new Button("Import your own fractal");
         Button createYourOwnButton = new Button("Create your own fractal");
+        Button quitButton = new Button("Quit");
 
 
         startButton.setOnAction(actionEvent -> {
                     SceneController sceneController = new SceneController(stage);
                     sceneController.switchScene(2);
                 });
+        quitButton.setOnAction(actionEvent -> {
+            Platform.exit();
+        });
 
-        VBox root = new VBox(title, infoBox, startButton, importButton, createYourOwnButton);
+        createYourOwnButton.setOnAction(actionEvent -> {
+            SceneController sceneController = new SceneController(stage);
+            sceneController.switchScene(4);
+        });
+        importButton.setOnAction(actionEvent -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Open fractal text file");
+            File file = fileChooser.showOpenDialog(stage);
+            try {
+                ChaosGameDescription chaosGameDescription = ChaosGameFileHandler.readFromFile(file.getPath());
+
+                SceneController sceneController = new SceneController(stage);
+                sceneController.setGameDescription(chaosGameDescription);
+                sceneController.setTransformations(Transformations.AFFINE2D);
+                sceneController.switchScene(3);
+
+            } catch (FileNotFoundException fnfe){
+
+            }
+        });
+
+        VBox root = new VBox(title, infoBox, startButton, importButton, createYourOwnButton, quitButton);
 
 
         scene = new Scene(root, 600, 600);

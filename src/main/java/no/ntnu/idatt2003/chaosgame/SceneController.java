96ea83b69e51acbd355ee8f1fd3c6a064ff263b2
abhi <abhiramk@stud.ntnu.no>
2024-05-19 20:47:02 +0200

    added setTransformations and setGameDescription method to SceneController class

--- src/main/java/no/ntnu/idatt2003/chaosgame/SceneController.java
+++ src/main/java/no/ntnu/idatt2003/chaosgame/SceneController.java
@@ -1,12 +1,16 @@
 package no.ntnu.idatt2003.chaosgame;
 
 import javafx.stage.Stage;
+import no.ntnu.idatt2003.chaosgame.components.ChaosGameDescription;
 import no.ntnu.idatt2003.chaosgame.scenes.CanvasScene;
 import no.ntnu.idatt2003.chaosgame.scenes.MenuScene;
 import no.ntnu.idatt2003.chaosgame.scenes.StartScene;
+import no.ntnu.idatt2003.chaosgame.transforms.Transformations;
 
 public class SceneController {
     private Stage stage;
+    private ChaosGameDescription chaosGameDescription;
+    private Transformations transformations;
     private final int MENU_SCENE = 1;
     private final int START_SCENE = 2;
     private final int CANVAS_SCENE = 3;
@@ -15,6 +19,14 @@ public class SceneController {
 
     public SceneController(Stage stage){
         this.stage = stage;
+    }
+
+    public void setGameDescription(ChaosGameDescription chaosGameDescription){
+        this.chaosGameDescription = chaosGameDescription;
+    }
+
+    public void setTransformations(Transformations transformations) {
+        this.transformations = transformations;
     }
 
     public void switchScene(int sceneNumber){
@@ -30,7 +42,7 @@ public class SceneController {
                 break;
             }
             case CANVAS_SCENE -> {
-                CanvasScene canvasScene = new CanvasScene(stage);
+                CanvasScene canvasScene = new CanvasScene(stage, chaosGameDescription, transformations);
                 stage.setScene(canvasScene.getScene());
                 break;
             }

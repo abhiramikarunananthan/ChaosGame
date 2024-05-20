35fdc2b8901417dc889ec11f52ceb68eae226a71
abhi <abhiramk@stud.ntnu.no>
2024-05-20 10:05:02 +0200

    Implemented a ChaosGameObserver interface with the method update() for checking if ChaosGame updates

---
+++ src/main/java/no/ntnu/idatt2003/chaosgame/components/ChaosGameObserver.java
@@ -0,0 +1,9 @@
+package no.ntnu.idatt2003.chaosgame.components;
+
+import no.ntnu.idatt2003.chaosgame.tensors.Vector2D;
+
+public interface ChaosGameObserver {
+
+    void update(Vector2D point);
+
+}

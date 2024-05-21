54e08bf2d4253a5fe9a221592edc4d2dd6ce07e3
abhi <abhiramk@stud.ntnu.no>
2024-05-21 16:31:13 +0200

    Added FunctionalInterface tag

--- src/main/java/no/ntnu/idatt2003/chaosgame/transforms/Transform2D.java
+++ src/main/java/no/ntnu/idatt2003/chaosgame/transforms/Transform2D.java
@@ -2,6 +2,7 @@ package no.ntnu.idatt2003.chaosgame.transforms;
 
 import no.ntnu.idatt2003.chaosgame.tensors.Vector2D;
 
+@FunctionalInterface
 public interface Transform2D {
     Vector2D transform(Vector2D point);
 

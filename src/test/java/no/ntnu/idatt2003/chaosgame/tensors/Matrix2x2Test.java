71c41518a545dde4146610121b84ffbe5db0c0fe
abhi <abhiramk@stud.ntnu.no>
2024-05-21 20:35:15 +0200

    Removed old tests

--- src/test/java/no/ntnu/idatt2003/chaosgame/tensors/Matrix2x2Test.java
+++ src/test/java/no/ntnu/idatt2003/chaosgame/tensors/Matrix2x2Test.java
@@ -1,7 +1,6 @@
 package no.ntnu.idatt2003.chaosgame.tensors;
 
-import no.ntnu.idatt2003.chaosgame.tensors.Matrix2x2;
-import no.ntnu.idatt2003.chaosgame.tensors.Vector2D;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,31 +8,89 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Matrix2x2Test {
 
-    Vector2D vector2d1;
-    Vector2D vector2d2;
-    Matrix2x2 matrix2x2;
+    private Matrix2x2 matrix2x2Test1;
+    private Matrix2x2 matrix2x2Test2;
+    private Vector2D vector2DTest1;
+    private Vector2D vector2DTest2;
+    private Matrix2x2 matrix2x2TestNull;
+
     @BeforeEach
     void setUp() {
-        vector2d1 = new Vector2D(3, 7);
-        vector2d2 = new Vector2D(-6, 10);
-        matrix2x2 = new Matrix2x2(3, 2, 4, 3);
+        vector2DTest1 = new Vector2D(1,7);
+        vector2DTest2 = new Vector2D(2,-3);
+
+        matrix2x2Test1 = new Matrix2x2(1,-4,0,2);
+        matrix2x2Test2 = new Matrix2x2(2,3,-5,-2);
+        matrix2x2TestNull = null;
     }
 
     @Test
     void multiply() {
+        Vector2D matrix1MultipliedVector1 = matrix2x2Test1.multiply(vector2DTest1);
+        Vector2D matrix1MultipliedVector2 = matrix2x2Test1.multiply(vector2DTest2);
 
-        Vector2D vector2DExpected1 = new Vector2D(23, 33);
-        Vector2D vector2DExpected2 = new Vector2D(2, 6);
+        Vector2D matrix2MultipliedVector1 = matrix2x2Test2.multiply(vector2DTest1);
+        Vector2D matrix2MultipliedVector2 = matrix2x2Test2.multiply(vector2DTest2);
 
+        Assertions.assertEquals(-27,matrix1MultipliedVector1.getX0());
+        Assertions.assertEquals(14, matrix1MultipliedVector1.getX1());
 
-        Vector2D vector2DProduct1 = matrix2x2.multiply(vector2d1);
-        Vector2D vector2DProduct2 = matrix2x2.multiply(vector2d2);
+        Assertions.assertEquals(14,matrix1MultipliedVector2.getX0());
+        Assertions.assertEquals(-6, matrix1MultipliedVector2.getX1());
 
+        Assertions.assertEquals(23,matrix2MultipliedVector1.getX0());
+        Assertions.assertEquals(-19,matrix2MultipliedVector1.getX1());
 
-        assertEquals(vector2DExpected1.getX0(), vector2DProduct1.getX0());
-        assertEquals(vector2DExpected1.getX1(), vector2DProduct1.getX1());
-        assertEquals(vector2DExpected2.getX0(), vector2DProduct2.getX0());
-        assertEquals(vector2DExpected2.getX1(), vector2DProduct2.getX1());
+        Assertions.assertEquals(-5,matrix2MultipliedVector2.getX0());
+        Assertions.assertEquals(-4,matrix2MultipliedVector2.getX1());
 
+        Assertions.assertThrows(NullPointerException.class, () -> matrix2x2TestNull.multiply(vector2DTest1));
+        Assertions.assertThrows(NullPointerException.class, () -> matrix2x2TestNull.multiply(vector2DTest2));
+    }
+
+    @Test
+    void getA00() {
+        double matrix1A00 = matrix2x2Test1.getA00();
+        double matrix2A00 = matrix2x2Test2.getA00();
+
+        Assertions.assertEquals(1, matrix1A00);
+        Assertions.assertEquals(2, matrix2A00);
+
+        Assertions.assertThrows(NullPointerException.class, () -> matrix2x2TestNull.getA00());
+
+    }
+
+    @Test
+    void getA01() {
+
+        double matrix1A01 = matrix2x2Test1.getA01();
+        double matrix2A01 = matrix2x2Test2.getA01();
+
+        Assertions.assertEquals(-4, matrix1A01);
+        Assertions.assertEquals(3, matrix2A01);
+
+        Assertions.assertThrows(NullPointerException.class, () -> matrix2x2TestNull.getA01());
+    }
+
+    @Test
+    void getA10() {
+        double matrix1A10 = matrix2x2Test1.getA10();
+        double matrix2A10 = matrix2x2Test2.getA10();
+
+        Assertions.assertEquals(0, matrix1A10);
+        Assertions.assertEquals(-5, matrix2A10);
+
+        Assertions.assertThrows(NullPointerException.class, () -> matrix2x2TestNull.getA10());
+    }
+
+    @Test
+    void getA11() {
+        double matrix1A11 = matrix2x2Test1.getA11();
+        double matrix2A11 = matrix2x2Test2.getA11();
+
+        Assertions.assertEquals(2, matrix1A11);
+        Assertions.assertEquals(-2, matrix2A11);
+
+        Assertions.assertThrows(NullPointerException.class, () -> matrix2x2TestNull.getA11());
     }
 }
--- src/test/java/no/ntnu/idatt2003/chaosgame/tensors/Vector2DTest.java
+++ src/test/java/no/ntnu/idatt2003/chaosgame/tensors/Vector2DTest.java
@@ -1,53 +1,129 @@
 package no.ntnu.idatt2003.chaosgame.tensors;
 
-import no.ntnu.idatt2003.chaosgame.tensors.Vector2D;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Vector2DTest {
-    Vector2D vector2d1;
-    Vector2D vector2d2;
+
+    private Vector2D vector2DTest1;
+    private Vector2D vector2DTest2;
+    private Vector2D vector2DTest3;
+    private Vector2D vector2DTestNull;
+
     @BeforeEach
     void setUp() {
-        vector2d1 = new Vector2D(3, 7);
-        vector2d2 = new Vector2D(-6, 10);
+        vector2DTest1 = new Vector2D(1,7);
+        vector2DTest2 = new Vector2D(2,-3);
+        vector2DTest3 = new Vector2D(-4,0);
+        vector2DTestNull = null;
+
     }
 
     @Test
     void getX0() {
-        double x0 = vector2d1.getX0();
+        double vector1X0 = vector2DTest1.getX0();
+        double vector2X0 = vector2DTest2.getX0();
+        double vector3X0 = vector2DTest3.getX0();
 
-        assertEquals(3, x0);
+        Assertions.assertEquals(1,vector1X0);
+        Assertions.assertNotEquals(7,vector1X0);
+
+        Assertions.assertEquals(2,vector2X0);
+        Assertions.assertNotEquals(-3,vector2X0);
+
+        Assertions.assertEquals(-4,vector3X0);
+        Assertions.assertNotEquals(0,vector3X0);
+
+        Assertions.assertThrows(NullPointerException.class,() -> vector2DTestNull.getX0());
+
     }
 
     @Test
     void getX1() {
-        double x1 = vector2d2.getX1();
+        double vector1X1 = vector2DTest1.getX1();
+        double vector2X1 = vector2DTest2.getX1();
+        double vector3X1 = vector2DTest3.getX1();
 
-        assertEquals(10, x1);
+
+        Assertions.assertEquals(7,vector1X1);
+        Assertions.assertNotEquals(1,vector1X1);
+
+        Assertions.assertEquals(-3,vector2X1);
+        Assertions.assertNotEquals(2,vector2X1);
+
+        Assertions.assertEquals(0,vector3X1);
+        Assertions.assertNotEquals(-4,vector3X1);
+
+        Assertions.assertThrows(NullPointerException.class,() -> vector2DTestNull.getX1());
     }
 
     @Test
     void add() {
-        Vector2D vectorExpected = new Vector2D(-3, 17);
+        Vector2D vector1PlusVector2 = vector2DTest1.add(vector2DTest2);
+        Vector2D vector2PlusVector1 = vector2DTest2.add(vector2DTest1);
 
-        Vector2D vectorSum = vector2d1.add(vector2d2);
+        Vector2D vector1PlusVector3 = vector2DTest1.add(vector2DTest3);
+        Vector2D vector3PlusVector1 = vector2DTest3.add(vector2DTest1);
 
-        assertEquals(vectorExpected.getX0(), vectorSum.getX0());
-        assertEquals(vectorExpected.getX1(), vectorSum.getX1());
+        Vector2D vector2PlusVector3 = vector2DTest2.add(vector2DTest3);
+        Vector2D vector3PlusVector2 = vector2DTest3.add(vector2DTest2);
 
+        Assertions.assertEquals(3,vector1PlusVector2.getX0());
+        Assertions.assertEquals(4,vector1PlusVector2.getX1());
+
+        Assertions.assertEquals(3,vector2PlusVector1.getX0());
+        Assertions.assertEquals(4,vector2PlusVector1.getX1());
+
+        Assertions.assertEquals(-3,vector1PlusVector3.getX0());
+        Assertions.assertEquals(7,vector1PlusVector3.getX1());
+
+        Assertions.assertEquals(-3,vector3PlusVector1.getX0());
+        Assertions.assertEquals(7,vector3PlusVector1.getX1());
+
+        Assertions.assertEquals(-2,vector2PlusVector3.getX0());
+        Assertions.assertEquals(-3,vector2PlusVector3.getX1());
+
+        Assertions.assertEquals(-2,vector3PlusVector2.getX0());
+        Assertions.assertEquals(-3,vector3PlusVector2.getX1());
+
+        Assertions.assertThrows(NullPointerException.class,() -> vector2DTest1.add(vector2DTestNull));
 
     }
 
     @Test
     void subtract() {
-        Vector2D vectorExpected = new Vector2D(9, -3);
 
-        Vector2D vectorSum = vector2d1.subtract(vector2d2);
+        Vector2D vector1MinusVector2 = vector2DTest1.subtract(vector2DTest2);
+        Vector2D vector2MinusVector1 = vector2DTest2.subtract(vector2DTest1);
 
-        assertEquals(vectorExpected.getX0(), vectorSum.getX0());
-        assertEquals(vectorExpected.getX1(), vectorSum.getX1());
+        Vector2D vector1MinusVector3 = vector2DTest1.subtract(vector2DTest3);
+        Vector2D vector3MinusVector1 = vector2DTest3.subtract(vector2DTest1);
+
+        Vector2D vector2MinusVector3 = vector2DTest2.subtract(vector2DTest3);
+        Vector2D vector3MinusVector2 = vector2DTest3.subtract(vector2DTest2);
+
+        Assertions.assertEquals(-1,vector1MinusVector2.getX0());
+        Assertions.assertEquals(10,vector1MinusVector2.getX1());
+
+        Assertions.assertEquals(1,vector2MinusVector1.getX0());
+        Assertions.assertEquals(-10,vector2MinusVector1.getX1());
+
+        Assertions.assertEquals(5,vector1MinusVector3.getX0());
+        Assertions.assertEquals(7,vector1MinusVector3.getX1());
+
+        Assertions.assertEquals(-5,vector3MinusVector1.getX0());
+        Assertions.assertEquals(-7,vector3MinusVector1.getX1());
+
+        Assertions.assertEquals(6,vector2MinusVector3.getX0());
+        Assertions.assertEquals(-3,vector2MinusVector3.getX1());
+
+        Assertions.assertEquals(-6,vector3MinusVector2.getX0());
+        Assertions.assertEquals(3,vector3MinusVector2.getX1());
+
+        Assertions.assertThrows(NullPointerException.class,() -> vector2DTest1.subtract(vector2DTestNull));
+
     }
 }

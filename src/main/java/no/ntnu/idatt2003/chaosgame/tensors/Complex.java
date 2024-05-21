9cba2a8af7b8ecfcf0a5ba93b33a3c87c28ff083
abhi <abhiramk@stud.ntnu.no>
2024-05-21 16:30:05 +0200

    Added a method for squaring a complex number

--- src/main/java/no/ntnu/idatt2003/chaosgame/tensors/Complex.java
+++ src/main/java/no/ntnu/idatt2003/chaosgame/tensors/Complex.java
@@ -5,6 +5,12 @@ public class Complex extends Vector2D {
         super(realPart, imaginaryPart);
     }
 
+    public Complex square(){
+        double newRealPart = this.getX0() * this.getX0();
+        double newImaginaryPart = this.getX1() * this.getX1();
+        return new Complex(newRealPart, newImaginaryPart);
+    }
+
 
     public Complex sqrt(){
        double newRealPart  = Math.sqrt(0.5*(Math.sqrt(Math.pow(this.getX0(), 2) +Math.pow(this.getX1(), 2) ) + this.getX0()));

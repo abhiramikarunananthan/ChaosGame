d23d6b65258807da01ae1ef380feb6448697b5a5
abhi <abhiramk@stud.ntnu.no>
2024-05-21 04:05:24 +0200

    Javadoc and exception handling

--- src/main/java/no/ntnu/idatt2003/chaosgame/components/ChaosGame.java
+++ src/main/java/no/ntnu/idatt2003/chaosgame/components/ChaosGame.java
@@ -1,11 +1,21 @@
 package no.ntnu.idatt2003.chaosgame.components;
 
+import no.ntnu.idatt2003.chaosgame.exceptions.NegativeDimensionsException;
 import no.ntnu.idatt2003.chaosgame.tensors.Vector2D;
 import no.ntnu.idatt2003.chaosgame.transforms.Transform2D;
 import no.ntnu.idatt2003.chaosgame.transforms.Transformations;
 
 import java.util.Random;
 
+/**
+ * A class representing the game object of
+ * a fractal game. The class contains the description
+ * of the fractal {@link #description}, and the
+ * canvas containing the fractal points {@link #canvas}.
+ *
+ * @author 10052
+ * @version 1.0
+ */
 public class ChaosGame {
     private ChaosCanvas canvas;
     private ChaosGameDescription description;
@@ -13,7 +23,11 @@ public class ChaosGame {
     public Random random;
 
 
-    public ChaosGame(ChaosGameDescription description, int height, int width) {
+    public ChaosGame(ChaosGameDescription description, int height, int width) throws NegativeDimensionsException {
+        if (height < 0 || width < 0) {
+            throw new NegativeDimensionsException("Width or height cannot be negative");
+        }
+
         this.description = description;
         this.canvas = new ChaosCanvas(width, height, description.getMinCoords(), description.getMaxCoords());
         this.currentPoint = new Vector2D(0, 0);

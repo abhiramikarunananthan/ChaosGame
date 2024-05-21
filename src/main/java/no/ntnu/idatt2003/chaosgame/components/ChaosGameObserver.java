996992b4dff0d5f89e31b523e08f5d684a84f13c
abhi <abhiramk@stud.ntnu.no>
2024-05-21 16:31:06 +0200

    Added FunctionalInterface tag

--- src/main/java/no/ntnu/idatt2003/chaosgame/components/ChaosGameObserver.java
+++ src/main/java/no/ntnu/idatt2003/chaosgame/components/ChaosGameObserver.java
@@ -2,6 +2,7 @@ package no.ntnu.idatt2003.chaosgame.components;
 
 import no.ntnu.idatt2003.chaosgame.tensors.Vector2D;
 
+@FunctionalInterface
 public interface ChaosGameObserver {
 
     void update(Vector2D point);

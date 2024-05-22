2c62827e5354c131fbe6c5749d1e17bd3d322362
abhi <abhiramk@stud.ntnu.no>
2024-05-22 12:31:50 +0200

    Javadoc for StartController

--- src/main/java/no/ntnu/idatt2003/chaosgame/controller/StartController.java
+++ src/main/java/no/ntnu/idatt2003/chaosgame/controller/StartController.java
@@ -22,30 +22,66 @@ public class StartController {
     private Button treeButton;
     private Button backButton;
 
+    /**
+     * Constructor for {@link StartController} class.
+     *
+     * @param stage The stage of the application.
+     */
     public StartController(Stage stage) {
         this.stage = stage;
     }
 
+    /**
+     * Set-method for the Sierpinski fractal button.
+     *
+     * @param sierpinskiButton The Sierpinski fractal button.
+     */
     public void setSierpinskiButton(Button sierpinskiButton) {
         this.sierpinskiButton = sierpinskiButton;
     }
 
+    /**
+     * Set-method for the Barnsley fern fractal button.
+     *
+     * @param barnsleyButton The Barnsley fern fractal button.
+     */
     public void setBarnsleyButton(Button barnsleyButton) {
         this.barnsleyButton = barnsleyButton;
     }
 
+    /**
+     * Set-method for the Julia fractal button.
+     *
+     * @param juliaButton The Julia fractal button.
+     */
     public void setJuliaButton(Button juliaButton) {
         this.juliaButton = juliaButton;
     }
 
+    /**
+     * Set-method for the back button for navigating to the main menu.
+     *
+     * @param backButton The back button.
+     */
     public void setBackButton(Button backButton) {
         this.backButton = backButton;
     }
 
+    /**
+     * Set-method for the Tree fractal button.
+     *
+     * @param treeButton The Tree fractal button.
+     */
     public void setTreeButton(Button treeButton) {
         this.treeButton = treeButton;
     }
 
+    /**
+     * Adds action listeners to the buttons.
+     *
+     * These listeners handle the button clicks and switch
+     * to the corresponding fractal scene or back to the main menu.
+     */
     public void addButtonListeners() {
 
         sierpinskiButton.setOnAction(actionEvent -> {
@@ -78,6 +114,12 @@ public class StartController {
         });
     }
 
+
+    /**
+     * Updates the scene with the specified root node.
+     *
+     * @param root The root node of the scene.
+     */
     public void updateScene(Parent root) {
         Scene scene = new Scene(root, 600, 600);
 
